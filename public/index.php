0da1600fe5ebb599f1d4b4e16ca8ec77529b9215
frnak.xu <xue_dong5437@126.com>
2015-09-02 20:50:35 +0800

    xyaf

--- public/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('APPLICATION_PATH', dirname(dirname(__FILE__)));
-
-$application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
-$application->bootstrap(null)->run();
